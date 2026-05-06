81cbf04995b5c137d51de8f98cb0501ebe73e1af
Jacob haywood <jacobthaywood@icloud.com>
2026-05-06 13:52:23 -0400

    Delete plot.js

--- plot.js
+++
@@ -1,6 +0,0 @@
-function f(x) {
-  return (x*x*x + 6.0*x + 12.0) / (x*x*x + 12.0*x + 48.0);
-}
-for(let x=0; x<=8; x++) {
-  console.log(`x=${x}, f=${f(x).toFixed(4)}, sig=${(1/(1+Math.exp(-x))).toFixed(4)}`);
-}
