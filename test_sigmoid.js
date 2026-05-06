3c4c0bd29ea0946489440dbff507217d52dc3d16
Jacob haywood <jacobthaywood@icloud.com>
2026-05-06 13:53:05 -0400

    Delete test_sigmoid.js

--- test_sigmoid.js
+++
@@ -1,9 +0,0 @@
-function _fastSigmoid(x) {
-  if (x >= 8.0) return 1.0;
-  if (x <= -8.0) return 0.0;
-  const x2 = x * x;
-  const x3 = x2 * x;
-  return (x3 + 6.0 * x + 12.0) / (x3 + 12.0 * x + 48.0);
-}
-console.log(_fastSigmoid(0));
-console.log(1 / (1 + Math.exp(0)));
